e05947cd98762a9ab0817c2be3ddc7229c1e2c2d
Mindexas <88283407+Mindexas@users.noreply.github.com>
2023-02-20 18:39:14 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,26 +6,9 @@ lua54 'yes'
 name 'CombatTagged'
 author 'Mindexas#0001'
 version 'v0.1'
-description 'A framework-standalone radio UI for FiveM'
-repository 'https://github.com/antond15/ac_radio'
-
-
-server_scripts {
-	'@async/async.lua',
-	'@oxmysql/lib/MySQL.lua',
-	'server/main.lua',
-}
+description 'A combat-tagged system for FiveM'
+repository 'https://github.com/Mindexas/mdx-combattag'
 
 client_scripts {
-	'@async/async.lua',
-	'@oxmysql/lib/MySQL.lua',
-	'client/redzone.lua',
-	'client/redzone2.lua',
 	'client/combat.lua',
 }
-
-shared_script '@ox_lib/init.lua'server_scripts { '@mysql-async/lib/MySQL.lua' }server_scripts { '@mysql-async/lib/MySQL.lua' }
-
-
-server_script '@salty_tokenizer/init.lua'
-client_script '@salty_tokenizer/init.lua'
